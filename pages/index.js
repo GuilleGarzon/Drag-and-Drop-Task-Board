5cc9bcd57ff7753dbbba2f2c69aaffafb071d9b2
GuilleGarzon <guillegarzon.gg@gmail.com>
2023-03-13 14:11:18 -0500

    feat: Add component Card

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import List from "../components/List"
+import Card from "../components/Card"
 
 export default function Home() {
   return (
@@ -5,6 +7,9 @@ export default function Home() {
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>  
+
+      <List />
+      <Card />
     </div>
   )
 }
